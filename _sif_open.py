b05e89b920b6e4bbcfb4fd105a4120e01c13f6d4
Manuel <manueleibl@web.de>
2020-04-29 14:17:07 +0200

    libraries added

---
+++ _sif_open.py
@@ -0,0 +1,238 @@
+import numpy as np
+from collections import OrderedDict
+
+# Read Andor Technology Multi-Channel files with PIL.
+# Based on Marcel Leutenegger's MATLAB script.
+
+_MAGIC = 'Andor Technology Multi-Channel File\n'
+
+# --------------------------------------------------------------------
+# SIF reader
+def _to_string(c):
+    ''' convert bytes to string. c: string or bytes'''
+    return c if not isinstance(c, bytes) else c.decode('utf-8')
+
+def _read_string(fp, length = None):
+    '''Read a string of the given length. If no length is provided, the
+    length is read from the file.'''
+    if length is None:
+        length = int(_to_string(fp.readline()))
+    return fp.read(length)
+
+def _read_until(fp, terminator=' '):
+    '''Read a space-delimited word.'''
+    word = ''
+    while True:
+        c = _to_string(fp.read(1))
+        if c == terminator or c == '\n':
+            if len(word) > 0:
+                break
+        word += c
+    return word
+
+def _read_int(fp):
+    return int(_read_until(fp, ' '))
+
+def _read_float(fp):
+    return float(_read_until(fp, ' '))
+
+def _open(fp):
+    """
+    A helper function to read SIF file.
+
+    Parameters
+    -----------
+    fp: File pointing to SIF file
+
+    Returns
+    -------
+    tile: list
+        A list of tuples, that contains the image location in the file.
+    size: a tuple, (wdith, height)
+    n_frames: integer
+        number of frames
+    info: dict
+        Dictionary containing misc data.
+    """
+    info = OrderedDict()
+
+    if _to_string(fp.read(36)) != _MAGIC:
+        raise SyntaxError('not a SIF file')
+
+    # What's this?
+    fp.readline() # 65538 number_of_images?
+
+    info['SifVersion'] = int(_read_until(fp, ' ')) # 65559
+
+    # What's this?
+    _read_until(fp, ' ') # 0
+    _read_until(fp, ' ') # 0
+    _read_until(fp, ' ') # 1
+
+    info['ExperimentTime'] = _read_int(fp)
+    info['DetectorTemperature'] = _read_float(fp)
+
+    # What is this?
+    _read_string(fp, 10) # blank
+
+    # What is this?
+    _read_until(fp, ' ') # 0
+
+    info['ExposureTime'] = _read_float(fp)
+    info['CycleTime'] = _read_float(fp)
+    info['AccumulatedCycleTime'] = _read_float(fp)
+    info['AccumulatedCycles'] = _read_int(fp)
+
+    fp.read(1) # NULL
+    fp.read(1) # space
+
+    info['StackCycleTime'] = _read_float(fp)
+    info['PixelReadoutTime'] = _read_float(fp)
+
+    # What is this?
+    _read_until(fp, ' ') # 0
+    _read_until(fp, ' ') # 1
+    info['GainDAC'] = _read_float(fp)
+
+    _read_until(fp, ' ')
+    _read_until(fp, ' ')
+
+    info['GateDelayStep'] = _read_float(fp)
+
+    # What is the rest of the line?
+    _read_until(fp, '\n')
+
+    info['DetectorType'] = _to_string(fp.readline())
+    info['DetectorDimensions'] = (_read_int(fp), _read_int(fp))
+    info['OriginalFilename'] = _read_string(fp)
+
+    # What is this?
+    fp.read(2) # space newline
+
+    # What is this?
+    _read_int(fp) # 65538
+    info['user_text'] = _read_string(fp)
+
+    fp.read(1) # newline
+    _read_int(fp) # 65538
+    fp.read(8) # 0x01 space 0x02 space 0x03 space 0x00 space
+    info['ShutterTime'] = (_read_float(fp), _read_float(fp)) # ends in newline
+
+    if (65548 <= info['SifVersion'] &
+            info['SifVersion'] <= 65557):
+        for _ in range(2):
+            fp.readline()
+    elif info['SifVersion'] == 65558:
+        for _ in range(5):
+            fp.readline()
+    elif info['SifVersion'] == 65559:
+        for _ in range(9):
+            fp.readline()
+    elif info['SifVersion'] == 65565:
+        for _ in range(15):
+            fp.readline()
+    elif info['SifVersion'] == 65567:
+        for _ in range(10):
+            fp.readline()
+        for _ in range(12):
+            _read_until(fp, ' ')
+        info['GateDelayStep'] = _read_float(fp)
+        for _ in range(8):
+            fp.readline()
+    elif info['SifVersion'] > 65567:
+        for _ in range(18):
+            fp.readline()
+
+    info['SifCalbVersion'] = int(_read_until(fp, ' ')) # 65539
+    # additional skip for this version
+    if info['SifCalbVersion'] == 65540:
+        fp.readline()
+
+    # 4th-order polynomial coefficients
+    info['Calibration_data'] = fp.readline()
+
+    fp.readline() # 0 1 0 0 newline
+    fp.readline() # 0 1 0 0 newline
+    fp.readline() # 0 1 0 0 newline
+
+    fp.readline() # 422 newline or 433 newline
+
+    fp.readline() # 13 newline
+    fp.readline() # 13 newline
+
+    info['FrameAxis'] = _read_string(fp)
+    info['DataType'] = _read_string(fp)
+    info['ImageAxis'] = _read_string(fp)
+
+    _read_until(fp, ' ') # 65541
+
+    _read_until(fp, ' ') # x0? left? -> x0
+    _read_until(fp, ' ') # x1? bottom? -> y1
+    _read_until(fp, ' ') # y1? right? -> x1
+    _read_until(fp, ' ') # y0? top? -> y0
+
+    no_images = int(_read_until(fp, ' '))
+    no_subimages = int(_read_until(fp, ' '))
+    total_length = int(_read_until(fp, ' '))
+    image_length = int(_read_until(fp, ' '))
+    info['NumberOfFrames'] = no_images
+
+    for i in range(no_subimages):
+        # read subimage information
+        _read_until(fp, ' ') # 65538
+
+        frame_area = fp.readline().strip().split()
+        x0, y1, x1, y0, ybin, xbin = map(int,frame_area[:6])
+        width = int((1 + x1 - x0) / xbin)
+        height = int((1 + y1 - y0) / ybin)
+    size = (int(width), int(height) * no_subimages)
+    tile = []
+
+    for f in range(no_images):
+        info['timestamp_of_{0:d}'.format(f)] = int(fp.readline())
+
+    offset = fp.tell()
+    try: # remove extra 0 if it exits.
+        flag = int(fp.readline())
+        if flag == 0:
+            offset = fp.tell()
+        # remove another extra 1
+        if flag == 1:
+            fp.readline()
+            offset = fp.tell()
+    except:
+        fp.seek(offset)
+
+    for f in range(no_images):
+        tile.append(("raw", (0, 0) + size,
+                     offset + f * width * height * no_subimages * 4,
+                     ('F;32F', 0, 1)))
+
+    info = extract_user_text(info)
+
+    return tile, size, no_images, info
+
+
+def extract_user_text(info):
+    """
+    Extract known information from info['user_text'].
+    Current known info is
+    + 'Calibration data for frame %d'
+    """
+    user_text = info['user_text']
+    if b'Calibration data for' in user_text[:20]:
+        texts = user_text.split(b'\n')
+        for i in range(info['NumberOfFrames']):
+            key = 'Calibration_data_for_frame_{:d}'.format(i+1)
+            coefs = texts[i][len(key)+2:].strip().split(b',')
+            info[key] = [float(c) for c in coefs]
+        # Calibration data should be None for this case
+        info['Calibration_data'] = None
+    else:
+        coefs = info['Calibration_data'].strip().split()
+        try:
+            info['Calibration_data'] = [float(c) for c in coefs]
+        except ValueError:
+            del info['Calibration_data']
+    del info['user_text']
+    return info
---
+++ trlfs.py
@@ -0,0 +1,498 @@
+"""
+Module for all the TRLFS functions.
+
+energy_correct: divides every value by the energy provided. For that a energydata file is needed in a tab separated
+                design with x = excitation wavelength and y = energy
+
+bg_corrector: draws a line between the lowest intensity in the range from 0 - 575nm and the lowest point between 575 - 581 nm
+                and subtracts this line from all points.
+
+norm_data: normalizes the data as
+            1) divided by max
+            2) F2 corrected (i.e. the max value in the range of 600 to the end of the data set
+            3) divided by the integrated are of the spectrum after interpolation between the data points
+
+
+"""
+import sif_reader
+from sif_reader import extract_calibration
+from scipy import interpolate
+from scipy import integrate
+
+
+def load_data(inp_data):
+    if inp_data.split('.')[-1] == 'sif':
+        data, info = sif_reader.np_open(inp_data)
+        xdata = extract_calibration(info)
+        ydata = data[0][0]
+    else:
+        xdata = []
+        ydata = []
+        with open(inp_data, 'r') as f:
+            data = f.readlines()
+        for line in data:
+            try:
+                splitted_line = line.split()
+                x = float(splitted_line[0])
+                y = float(splitted_line[1])
+                xdata.append(x)
+                ydata.append(y)
+            except:
+                try:
+                    splitted_line = line.split(',')
+                    x = float(splitted_line[0])
+                    y = float(splitted_line[1])
+                    xdata.append(x)
+                    ydata.append(y)
+                except:
+                    splitted_line = line.split()
+                    x_with_comma = splitted_line[0].split(',')
+                    x_with_dot = x_with_comma[0] + '.' + x_with_comma[1]
+                    x = float(x_with_dot)
+                    y = float(splitted_line[1])
+                    xdata.append(x)
+                    ydata.append(y)
+    return xdata, ydata
+
+def lifetime_handler(inp_data):
+    try:
+        data, info = sif_reader.np_open(inp_data)
+        data_fail_test = data[1][0] # Will fail if it is a single .sif spectrum
+        return data, info
+    except:
+        return None
+
+def energy_corrector(data, i, xdata, energydata):
+    out_energy_corr_data = []
+    with open(energydata, 'r') as f:
+        content = f.readlines()
+        e_xdata = []
+        e_ydata = []
+        for line in content:
+            splitted_line = line.split()
+            x = float(splitted_line[0])
+            y = float(splitted_line[1])
+            e_xdata.append(x)
+            e_ydata.append(y)
+    if xdata != e_xdata:
+        print('Warning, Energydata and x axis dont match!')
+    else:
+        for j,value in enumerate(data):
+            out_energy_corr_data.append(value/e_ydata[j])
+    return out_energy_corr_data
+
+def bg_correction(data, xdata):
+    out_bg_corr_data = []
+    for i,value in enumerate(xdata):
+        if value > 575:
+            low_range_max = i
+            break
+    for j,value in enumerate(xdata):
+        if value > 581:
+            high_range_min = j
+            break
+    #y = ax + b
+    low_min_x = xdata[data.index(min(data[0:low_range_max]))]
+    high_min_x = xdata[data.index(min(data[high_range_min:]))]
+    a = (min(data[high_range_min:])-min(data[0:low_range_max]))/(high_min_x-low_min_x)
+    b = min(data[0:low_range_max])-a*low_min_x
+
+    for i,value in enumerate(data):
+        out_bg_corr_data.append(value-(a*xdata[i]+b))
+    return out_bg_corr_data
+
+def norm_data(data, decider, xdata=[], xlowlim=None, xuplim=None):
+    out_norm_data = []
+    if decider == 1:
+        for value in data:
+            out_value = value/max(data)
+            out_norm_data.append(out_value)
+        return out_norm_data
+
+    elif decider == 2:
+        for i,value in enumerate(xdata):
+            if value > 600:
+                low_value = i
+                break
+        for value in data:
+            out_value = value/max(data[low_value:])
+            out_norm_data.append(out_value)
+        return out_norm_data
+
+    elif decider ==3:
+        if xlowlim != None:
+            if xlowlim < xdata[0]:
+                print(f'lower limit {xlowlim} out of range\n'
+                f'min value is {xdata[0]}')
+            elif xuplim > xdata[-1]:
+                print(f'upper limit {xuplim} out of range\n'
+                f'max value is {xdata[-1]}')
+                xuplim = xdata[-1]
+            for i,value in enumerate(xdata):
+                if value > xlowlim:
+                    low_value = i
+                    break
+            for j,value in enumerate(xdata):
+                if value >= xuplim:
+                    upper_value = j-1
+                    break
+            interpolate_function = interpolate.interp1d(xdata[low_value:upper_value], data[low_value:upper_value])
+            integral = integrate.quad(interpolate_function, xdata[low_value], xdata[upper_value-1], limit=500)
+            for value in data:
+                out_norm_data.append(value/integral[0])
+        else:
+            interpolate_function = interpolate.interp1d(xdata, data)
+            integral = integrate.quad(interpolate_function, xdata[0], xdata[-1], limit=500)
+
+            for value in data:
+                out_norm_data.append(value/integral[0])
+        return out_norm_data
+
+def deconvolute_data(data1, data2, fraction=1):
+    out_deconv_data = []
+    for i,value in enumerate(data1):
+        out_value = float(value) - float(data2[i])*fraction
+        out_deconv_data.append(out_value)
+    return out_deconv_data
+
+def Ne_corrector(xdata, Ne_data, ref_Ne_data):
+    if Ne_data != ref_Ne_data:
+        with open(ref_Ne_data, 'r') as f:
+            content = f.readlines()
+            Ne_ref_xdata = []
+            Ne_ref_ydata = []
+            for line in content:
+                splitted_line = line.split()
+                x = float(splitted_line[0])
+                y = float(splitted_line[1])
+                Ne_ref_xdata.append(x)
+                Ne_ref_ydata.append(y)
+        with open(Ne_data, 'r') as f:
+            content = f.readlines()
+            Ne_xdata = []
+            Ne_ydata = []
+            for line in content:
+                splitted_line = line.split()
+                x = float(splitted_line[0])
+                y = float(splitted_line[1])
+                Ne_xdata.append(x)
+                Ne_ydata.append(y)
+        for i,value in enumerate(Ne_ref_xdata):
+            if value > 612:
+                x_ref_lower_limit = i
+                break
+        for j, value in enumerate(Ne_ref_xdata):
+            if value > 615:
+                x_ref_upper_limit = j
+                break
+
+        ref_max = Ne_ref_ydata.index(max(Ne_ref_ydata[x_ref_lower_limit: x_ref_upper_limit]))
+
+        for k,value in enumerate(Ne_xdata):
+            if value > 612:
+                x_lower_limit = k
+                break
+        for n, value in enumerate(Ne_xdata):
+            if value > 615:
+                x_upper_limit = n
+                break
+        Ne_max = Ne_ydata.index(max(Ne_ydata[x_lower_limit: x_upper_limit]))
+        Ne_x_corr = []
+        for value in xdata:
+            Ne_x_corr.append(value + Ne_xdata[Ne_max] - Ne_ref_xdata[ref_max])
+
+    else:
+        Ne_x_corr = xdata
+
+    return Ne_x_corr
+
+
+
+def x_corrector(xdata):
+    input_list = []
+    for item in xdata:
+        input_list.append(int(100*float(item)))
+    output = []
+    for value in input_list:
+        if value in range(57200,57310):
+            corr_value = value - 11
+            output.append(float(corr_value/100))
+        elif value in range(57310,57350):
+            corr_value = value - 12
+            output.append(float(corr_value/100))
+        elif value in range(57350,57550):
+            corr_value = value - 13
+            output.append(float(corr_value/100))
+        elif value in range(57550,57610):
+            corr_value = value - 14
+            output.append(float(corr_value/100))
+        elif value in range(57610,57735):
+            corr_value = value - 15
+            output.append(float(corr_value/100))
+        elif value in range(57735,57830):
+            corr_value = value - 16
+            output.append(float(corr_value/100))
+        elif value in range(57830,57940):
+            corr_value = value - 17
+            output.append(float(corr_value/100))
+        elif value in range(57940,58070):
+            corr_value = value - 18
+            output.append(float(corr_value/100))
+        elif value in range(58070,58225):
+            corr_value = value - 19
+            output.append(float(corr_value/100))
+        elif value in range(58225,58400):
+            corr_value = value - 20
+            output.append(float(corr_value/100))
+        elif value in range(58400,59130):
+            corr_value = value - 21
+            output.append(float(corr_value/100))
+        elif value in range(59130,59275):
+            corr_value = value - 22
+            output.append(float(corr_value/100))
+        elif value in range(59275,60355):
+            corr_value = value - 23
+            output.append(float(corr_value/100))
+        elif value in range(60355,60500):
+            corr_value = value - 22
+            output.append(float(corr_value/100))
+        elif value in range(60500,60775):
+            corr_value = value - 21
+            output.append(float(corr_value/100))
+        elif value in range(60775,61140):
+            corr_value = value - 20
+            output.append(float(corr_value/100))
+        elif value in range(61140,61300):
+            corr_value = value - 19
+            output.append(float(corr_value/100))
+        elif value in range(61300,61435):
+            corr_value = value - 18
+            output.append(float(corr_value/100))
+        elif value in range(61435,61500):
+            corr_value = value - 17
+            output.append(float(corr_value/100))
+        elif value in range(61500,61900):
+            corr_value = value - 15
+            output.append(float(corr_value/100))
+        elif value in range(61900,62000):
+            corr_value = value - 14
+            output.append(float(corr_value/100))
+        elif value in range(62000,62100):
+            corr_value = value - 13
+            output.append(float(corr_value/100))
+        elif value in range(62100,62200):
+            corr_value = value - 12
+            output.append(float(corr_value/100))
+        elif value in range(62200,62305):
+            corr_value = value - 11
+            output.append(float(corr_value/100))
+        elif value in range(62305,62405):
+            corr_value = value - 1
+            output.append(float(corr_value/100))
+        elif value in range(62405,62450):
+            corr_value = value - 9
+            output.append(float(corr_value/100))
+        elif value in range(62450,62550):
+            corr_value = value - 8
+            output.append(float(corr_value/100))
+        elif value in range(62550,62650):
+            corr_value = value - 7
+            output.append(float(corr_value/100))
+        elif value in range(62650,62750):
+            corr_value = value - 6
+            output.append(float(corr_value/100))
+        elif value in range(62750,62840):
+            corr_value = value - 5
+            output.append(float(corr_value/100))
+        elif value in range(62840,62900):
+            corr_value = value - 4
+            output.append(float(corr_value/100))
+        elif value in range(62900,62950):
+            corr_value = value - 3
+            output.append(float(corr_value/100))
+        elif value in range(62950,63050):
+            corr_value = value - 2
+            output.append(float(corr_value/100))
+        elif value in range(63050,63155):
+            corr_value = value - 1
+            output.append(float(corr_value/100))
+        elif value in range(63155,63225):
+            corr_value = value
+            output.append(float(corr_value/100))
+        elif value in range(62650,63255):
+            corr_value = value + 1
+            output.append(float(corr_value/100))
+        elif value in range(63255,63405):
+            corr_value = value + 2
+            output.append(float(corr_value/100))
+        elif value in range(63405,63450):
+            corr_value = value + 3
+            output.append(float(corr_value/100))
+        elif value in range(63450,63575):
+            corr_value = value + 4
+            output.append(float(corr_value/100))
+        elif value in range(63575,63625):
+            corr_value = value + 5
+            output.append(float(corr_value/100))
+        elif value in range(63625,63725):
+            corr_value = value + 6
+            output.append(float(corr_value/100))
+        elif value in range(63725,63775):
+            corr_value = value + 7
+            output.append(float(corr_value/100))
+        elif value in range(63775,63875):
+            corr_value = value + 8
+            output.append(float(corr_value/100))
+        elif value in range(63875,63925):
+            corr_value = value + 9
+            output.append(float(corr_value/100))
+        elif value in range(63925,63975):
+            corr_value = value + 1
+            output.append(float(corr_value/100))
+        elif value in range(63975,64030):
+            corr_value = value + 11
+            output.append(float(corr_value/100))
+        elif value in range(64030,64090):
+            corr_value = value + 12
+            output.append(float(corr_value/100))
+        elif value in range(64090,64175):
+            corr_value = value + 13
+            output.append(float(corr_value/100))
+        elif value in range(64175,64225):
+            corr_value = value + 14
+            output.append(float(corr_value/100))
+        elif value in range(64225,64275):
+            corr_value = value + 15
+            output.append(float(corr_value/100))
+        elif value in range(64275,64325):
+            corr_value = value + 16
+            output.append(float(corr_value/100))
+        elif value in range(64325,64375):
+            corr_value = value + 17
+            output.append(float(corr_value/100))
+        elif value in range(64375,64425):
+            corr_value = value + 18
+            output.append(float(corr_value/100))
+        elif value in range(64425,64525):
+            corr_value = value + 19
+            output.append(float(corr_value/100))
+        elif value in range(64525,64625):
+            corr_value = value + 20
+            output.append(float(corr_value/100))
+        elif value in range(64625,64675):
+            corr_value = value + 21
+            output.append(float(corr_value/100))
+        elif value in range(64675,64720):
+            corr_value = value + 22
+            output.append(float(corr_value/100))
+        elif value in range(64720,64740):
+            corr_value = value + 23
+            output.append(float(corr_value/100))
+        elif value in range(64740,64775):
+            corr_value = value + 24
+            output.append(float(corr_value/100))
+        elif value in range(64775,64825):
+            corr_value = value + 25
+            output.append(float(corr_value/100))
+        elif value in range(64825,64875):
+            corr_value = value + 26
+            output.append(float(corr_value/100))
+        elif value in range(64875,64925):
+            corr_value = value + 27
+            output.append(float(corr_value/100))
+        elif value in range(64925,64975):
+            corr_value = value + 28
+            output.append(float(corr_value/100))
+        elif value in range(64975,65090):
+            corr_value = value + 29
+            output.append(float(corr_value/100))
+        elif value in range(65090,65125):
+            corr_value = value + 3
+            output.append(float(corr_value/100))
+        elif value in range(65125,65175):
+            corr_value = value + 31
+            output.append(float(corr_value/100))
+        elif value in range(65175,65225):
+            corr_value = value + 32
+            output.append(float(corr_value/100))
+        elif value in range(65225,65275):
+            corr_value = value + 33
+            output.append(float(corr_value/100))
+        elif value in range(65275,65360):
+            corr_value = value + 34
+            output.append(float(corr_value/100))
+        elif value in range(65360,65410):
+            corr_value = value + 35
+            output.append(float(corr_value/100))
+        elif value in range(65410,65450):
+            corr_value = value + 36
+            output.append(float(corr_value/100))
+        elif value in range(65450,65500):
+            corr_value = value + 37
+            output.append(float(corr_value/100))
+        elif value in range(65500,65550):
+            corr_value = value + 38
+            output.append(float(corr_value/100))
+        elif value in range(65550,65610):
+            corr_value = value + 39
+            output.append(float(corr_value/100))
+        elif value in range(65610,65640):
+            corr_value = value + 4
+            output.append(float(corr_value/100))
+        elif value in range(65640,65710):
+            corr_value = value + 41
+            output.append(float(corr_value/100))
+        elif value in range(65710,65750):
+            corr_value = value + 42
+            output.append(float(corr_value/100))
+        elif value in range(65750,65810):
+            corr_value = value + 43
+            output.append(float(corr_value/100))
+        elif value in range(65810,65835):
+            corr_value = value + 44
+            output.append(float(corr_value/100))
+        elif value in range(65835,65845):
+            corr_value = value + 45
+            output.append(float(corr_value/100))
+        elif value in range(65850,65900):
+            corr_value = value + 46
+            output.append(float(corr_value/100))
+        elif value in range(65900,65925):
+            corr_value = value + 47
+            output.append(float(corr_value/100))
+        elif value in range(65925,65975):
+            corr_value = value + 48
+            output.append(float(corr_value/100))
+        elif value in range(65975,66025):
+            corr_value = value + 49
+            output.append(float(corr_value/100))
+        elif value in range(66025,66075):
+            corr_value = value + 50
+            output.append(float(corr_value/100))
+        elif value in range(66075,66125):
+            corr_value = value + 51
+            output.append(float(corr_value/100))
+        elif value in range(66125,66375):
+            corr_value = value + 52
+            output.append(float(corr_value/100))
+        elif value in range(66375,66475):
+            corr_value = value + 51
+            output.append(float(corr_value/100))
+        elif value in range(66475,66525):
+            corr_value = value + 50
+            output.append(float(corr_value/100))
+        elif value in range(66525,66675):
+            corr_value = value + 49
+            output.append(float(corr_value/100))
+        elif value in range(66675,66725):
+            corr_value = value + 48
+            output.append(float(corr_value/100))
+        elif value in range(66725,66775):
+            corr_value = value + 47
+            output.append(float(corr_value/100))
+        elif value in range(66775,66805):
+            corr_value = value + 46
+            output.append(float(corr_value/100))
+        else:
+            output.append('Not in Range')
+    return output
